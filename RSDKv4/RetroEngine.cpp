88f94a39b1e8ffc0cdd23ed8e4bd2c25ff874b5b
Ash Logan <ash@heyquark.com>
2021-10-23 00:08:05 +1100

    wiiu: Add basic logging
    RSDK already makes a logfile, but this is easier in some situations

--- RSDKv4/RetroEngine.cpp
+++ RSDKv4/RetroEngine.cpp
@@ -240,10 +240,33 @@ bool processEvents()
     return true;
 }
 
+#if RETRO_PLATFORM == RETRO_WIIU
+#include <whb/log.h>
+#include <whb/log_udp.h>
+#include <whb/log_cafe.h>
+#include <unistd.h>
+#include <sys/iosupport.h>
+
+static ssize_t wiiu_log_write(struct _reent* r, void* fd, const char* ptr, size_t len) {
+    WHBLogPrintf("%*.*s", len, len, ptr);
+    return len;
+}
+static const devoptab_t dotab_stdout = {
+    .name = "stdout_whb",
+    .write_r = wiiu_log_write,
+};
+#endif
+
 void RetroEngine::Init()
 {
 #if RETRO_PLATFORM == RETRO_ANDROID
     sleep(1); // wait to initialize the engine
+#endif
+#if RETRO_PLATFORM == RETRO_WIIU
+    WHBLogUdpInit();
+    WHBLogCafeInit();
+    devoptab_list[STD_OUT] = &dotab_stdout;
+    devoptab_list[STD_ERR] = &dotab_stdout;
 #endif
 
     CalculateTrigAngles();
